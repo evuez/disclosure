cb4603ea66a473a7be891a987fcf5cfaa4049738
evuez <hello@evuez.net>
2014-03-24 14:51:58 +0100

    add command bar

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import Tkinter as tk
 import tkFont
+import re
 from uuid import uuid4
 from collections import Counter
 from generic import Point, Size, color_variant, distance
@@ -12,7 +13,7 @@ import things
 AREA_HEIGHT = 630
 THING_COUNT = 5
 SHADOW_COLOR = (0, 0, 0)
-INVENTORY = Size(AREA_HEIGHT, 60)
+INVENTORY = Size(60, AREA_HEIGHT)
 
 
 class NotWideEnoughException(Exception):
@@ -27,29 +28,9 @@ class Game(tk.Frame):
 			)
 
 		tk.Frame.__init__(self, parent)
-		self.canvas = tk.Canvas(
-			self,
-			highlightthickness=0,
-			width=height,
-			height=height,
-			background='white'
-		)
-		self.canvas.pack(side='top', fill='both', expand=True, padx=2, pady=2)
+		tk.Frame.config(self, background='white')
 
-		self.inventory = tk.Canvas(
-			self,
-			highlightthickness=0,
-			width=INVENTORY.w,
-			height=INVENTORY.h,
-			background='black'
-		)
-		self.inventory.pack(
-			side='bottom',
-			fill='both',
-			expand=True,
-			padx=2,
-			pady=2
-		)
+		self.init_frames(height)
 
 		self.player = things.Player()
 		self.new()
@@ -72,14 +53,43 @@ class Game(tk.Frame):
 			name='font{}'.format(size)
 		)
 
-	def new(self):
+	def init_frames(self, height):
+		self.canvas = tk.Canvas(
+			self,
+			highlightthickness=0,
+			width=height,
+			height=height,
+			background='black'
+		)
+		self.inventory = tk.Canvas(
+			self,
+			highlightthickness=0,
+			width=INVENTORY.w,
+			height=INVENTORY.h,
+			background='black'
+		)
+
+		self.show_command()
+
+		self.inventory.pack(
+			side='left',
+			expand=True,
+			padx=1,
+			pady=2
+		)
+		self.canvas.pack(side='left', expand=True, padx=1, pady=2)
+
+	def new(self, seed=None):
 		self.seed = '{}-{}'.format (str(uuid4())[:8], self.player.level)
+		if seed is not None:
+			self.seed = seed
 
 		self.player.refresh()
 		self.canvas.delete('all')
 		self.area = Area(self.thing_count, self.thing_count, self.seed)
 		self.draw_area()
 		self.draw_inventory()
+		self.update_command()
 
 	def draw_area(self):
 		for y,row in enumerate(self.area.grid):
@@ -115,6 +125,8 @@ class Game(tk.Frame):
 		self.canvas.bind_all('<KeyPress-Down>', self.move_player)
 		self.canvas.bind_all('<KeyPress-Left>', self.move_player)
 		self.canvas.bind_all('<KeyPress-Right>', self.move_player)
+
+		self.command_entry.bind('<Return>', self.do_command)
 
 	def move_player(self, event):
 		x = y = 0
@@ -202,22 +214,22 @@ class Game(tk.Frame):
 
 	def draw_inventory_thing(self, thing_class, count, slot):
 		self.inventory.create_rectangle(
-			slot * INVENTORY.h,
-			INVENTORY.h,
-			slot * INVENTORY.h + INVENTORY.h,
 			0,
+			slot * INVENTORY.w,
+			INVENTORY.w,
+			slot * INVENTORY.w + INVENTORY.w,
 			width=0,
 			fill='#{0:02x}{1:02x}{2:02x}'.format(*thing_class.COLOR)
 		)
 		self.inventory.create_text(
-			slot * INVENTORY.h + INVENTORY.h / 2,
-			9,
+			INVENTORY.w / 2,
+			slot * INVENTORY.w + 9,
 			text=thing_class.__name__,
 			font=self.font(size=7, weight='normal')
 		)
 		self.inventory.create_text(
-			slot * INVENTORY.h + INVENTORY.h / 2,
-			INVENTORY.h / 2,
+			INVENTORY.w / 2,
+			slot * INVENTORY.w + INVENTORY.w / 2,
 			text=count,
 			font=self.font(size=14, weight='bold')
 		)
@@ -228,6 +240,36 @@ class Game(tk.Frame):
 		for slot,thing in enumerate(Counter(thing_classes).items()):
 			self.draw_inventory_thing(*thing, slot=slot)
 
+	def show_command(self):
+		self.command_entry = tk.Entry(
+			self,
+			borderwidth=2,
+			relief='flat',
+			background='black',
+			foreground='white',
+			font=self.font(size=11, weight='normal'),
+			insertbackground='white'
+		)
+		self.command_entry.pack(side='top', fill='x', padx=0, pady=0)
+
+	def update_command(self):
+		self.command_entry.delete(0, 'end')
+		self.command_entry.insert(0, '> use {}'.format(self.seed))
+
+	def do_command(self, event):
+		cmd = self.command_entry.get()
+		cmd = cmd.replace('>', '').strip()
+		if cmd.startswith('use '):
+			seed = cmd.replace('use', '').strip()
+			level = re.search(r'(-[0-9]+)$', seed)
+			if level is not None:
+				self.player.level = int(level.group()[1:])
+			self.new(seed)
+		elif cmd == 'reload' or cmd == 'new':
+			self.new()
+
+
+
 
 # a bell ring, when approching it rings louder, to indicate direction
 if __name__ == '__main__':
@@ -235,6 +277,6 @@ if __name__ == '__main__':
 	root.resizable(0, 0)
 
 	game = Game(root)
-	game.pack(side='left', padx=4, pady=4)
+	game.pack(side='left', padx=0, pady=0)
 
 	root.mainloop()
